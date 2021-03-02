829188918d2697f90eefbde5fcddf407e351dd26
Jana <jana.yj.orth@gmail.com>
2021-03-02 13:48:32 +0100

    medium leetcode boxes

--- 06-mediumLC/main.go
+++ 06-mediumLC/main.go
@@ -1,14 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println(minOperations("min"))
+	fmt.Println(minOperations("110"))
 }
 
 //1769: Minimum Number of Operations to Move All Balls to Each Box
 // You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
+// 388ms 100% faster, 4.9MB 100% less memory usage
 func minOperations(boxes string) []int {
-	res := []int{1, 2}
-	return res
+
+	//use array here because length of result is equal to length or boxes
+	result := make([]int, len(boxes))
+	var counter int
+
+	for i := range boxes {
+		for j, v := range boxes {
+			if i != j {
+				//string conversion here because input is of type string
+				if string(v) == "1" {
+
+					if j < i {
+						counter += i - j
+					}
+
+					if j > i {
+						counter += j - i
+					}
+
+				}
+			}
+		}
+		result[i] = counter
+		counter = 0
+	}
+
+	return result
 }
--- 06-mediumLC/main_test.go
+++ 06-mediumLC/main_test.go
@@ -1,7 +1,25 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestMinOperations(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  []int
+	}{
+		{"110", []int{1, 1, 3}},
+		{"001011", []int{11, 8, 5, 4, 3, 4}},
+	}
+
+	for _, tt := range tests {
+		got := minOperations(tt.input)
+
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("got %v, want %v", got, tt.want)
+		}
+	}
 
 }
